ba381bd1b68a440abc43cd0c9dea77c991629e59
Jan Daniel <jan.daniel@datacruit.com>
2025-08-31 22:42:03 +0200

    Minor change

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ consumer_task: Optional[asyncio.Task] = None
 # --------------------
 BASE_MOCKS: List[Dict] = [
     dict(
-        id=105,
+        id=9876,
         sensor_identification="meter-01",
         description="Main corridor smart meter",
         power=120.5,     # Watts
@@ -90,7 +90,7 @@ BASE_MOCKS: List[Dict] = [
         room_id=1000
     ),
     dict(
-        id=106,
+        id=9877,
         sensor_identification="meter-02",
         description="Kitchen meter",
         power=850.2,
@@ -100,7 +100,7 @@ BASE_MOCKS: List[Dict] = [
         room_id=1000
     ),
     dict(
-        id=107,
+        id=9878,
         sensor_identification="meter-03",
         description="Server room meter",
         power=1540.0,
@@ -110,7 +110,7 @@ BASE_MOCKS: List[Dict] = [
         room_id=1000
     ),
     dict(
-        id=108,
+        id=9879,
         sensor_identification="meter-04",
         description="Office lighting circuit",
         power=420.0,
